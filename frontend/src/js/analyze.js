c3e141e52f69a4232cbe903f1ad4fd6c3025b0e4
thiagola92 <thiagola92@gmail.com>
2026-03-18 11:38:41 -0300

    Update input field from frontend

--- frontend/src/js/analyze.js
+++ frontend/src/js/analyze.js
@@ -1,5 +1,36 @@
 
-function analyze() {
-    let url = urlField.value
-    console.log(url)
+async function onAnalyzeRequested() {
+    let url = urlField.value;
+    let response = await fetch("http://127.0.0.1:8000/analyze", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "url": url,
+        })
+    })
+
+    if (!response.ok) {
+        console.log(response)
+        urlField.removeAttribute("aria-invalid")
+        resultText.textContent = "Falha na comunicação com o server."
+        return
+    }
+
+    content = await response.json();
+
+    if (content["is_legit"] == true) {
+        urlField.setAttribute("aria-invalid", "false");
+        resultText.textContent = "Parece legitimo"
+    } else {
+        urlField.setAttribute("aria-invalid", "true");
+        resultText.textContent = "Parece phishing"
+    }
 }
+
+function onWindowLoad() {
+    analyzeButton.addEventListener("click", onAnalyzeRequested);
+}
+
+window.onload = onWindowLoad
